0d289d80fdd487df5aa1767e93c208c80ec4767e
rgauci <rgauci@users.noreply.github.com>
2015-08-18 00:16:16 +0100

    Create config.login.inc.php

---
+++ config.login.inc.php
@@ -0,0 +1,53 @@
+<?php 
+
+    // Code based on tutorials at: 
+    // http://www.mybringback.com/android-sdk/12924/android-tutorial-using-remote-databases-php-and-mysql-part-1/
+    // http://forums.devshed.com/php-faqs-stickies-167/program-basic-secure-login-system-using-php-mysql-891201.html
+
+    // These variables define the login information for the track researcher
+    $researcher_username = "researcher_username";
+    $researcher_password = "researcher_password";
+    
+    // This block of code is used to undo magic quotes.  Magic quotes are a terrible 
+    // feature that was removed from PHP as of PHP 5.4.  However, older installations 
+    // of PHP may still have magic quotes enabled and this code is necessary to 
+    // prevent them from causing problems.  For more information on magic quotes: 
+    // http://php.net/manual/en/security.magicquotes.php 
+    if(function_exists('get_magic_quotes_gpc') && get_magic_quotes_gpc()) 
+    { 
+        function undo_magic_quotes_gpc(&$array) 
+        { 
+            foreach($array as &$value) 
+            { 
+                if(is_array($value)) 
+                { 
+                    undo_magic_quotes_gpc($value); 
+                } 
+                else 
+                { 
+                    $value = stripslashes($value); 
+                } 
+            } 
+        } 
+     
+        undo_magic_quotes_gpc($_POST); 
+        undo_magic_quotes_gpc($_GET); 
+        undo_magic_quotes_gpc($_COOKIE); 
+    } 
+     
+    // This tells the web browser that your content is encoded using UTF-8 
+    // and that it should submit content back to you using UTF-8 
+    header('Content-Type: text/html; charset=utf-8'); 
+     
+    // This initializes a session.  Sessions are used to store information about 
+    // a visitor from one web page visit to the next.  Unlike a cookie, the information is 
+    // stored on the server-side and cannot be modified by the visitor.  However, 
+    // note that in most cases sessions do still use cookies and require the visitor 
+    // to have cookies enabled.  For more information about sessions: 
+    // http://us.php.net/manual/en/book.session.php 
+    session_start(); 
+
+    // Note that it is a good practice to NOT end your PHP files with a closing PHP tag. 
+    // This prevents trailing newlines on the file from being included in your output, 
+    // which can cause problems with redirecting users.
+?>
